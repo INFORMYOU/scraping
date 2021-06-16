af91be36ccc658eb4dd71a7e2af2a41c9196fe6f
jungeun0121 <dodamdodam121@gmail.com>
2021-06-16 23:41:22 +0900

    #10) 상품 상세 정보 스크래핑 코드 구현

--- DetailDealInfo.py
+++ DetailDealInfo.py
@@ -1,5 +1,11 @@
+#Step 0. 필요한 모듈과 라이브러리를 로딩합니다.
 import pymysql
+import requests # Python에서 HTTP 요청을 보내는 모듈
+from bs4 import BeautifulSoup # 데이터 파싱
+import time
+import math
 
+# get url addresses of recent deals from the views_tb table.
 def read_url():
 
     #Step 1. connection information
@@ -14,8 +20,8 @@ def read_url():
     #Step 2. create cursor
     curs = conn.cursor()
 
-    #Step 3.get url info
-    sql = 'select url from views_tb order by time desc limit 10;'
+    #Step 3.get recent deal's url list
+    sql = 'select url from views_tb order by time desc limit 20;'
     curs.execute(sql)
     duplicated_urls = curs.fetchall()
 
@@ -27,15 +33,48 @@ def read_url():
     #Step 4. close connection
     conn.close()
 
-    return get_detailDealInfo(urls)
+    # #Step 5. get each deal address
+    for i in urls :
+        url = str(i)[2:-3]
+        get_detailDealInfo(url)
 
 
-def get_detailDealInfo(urls):
-
+# get each deal's detail info
+def get_detailDealInfo(URL) :
     
-    print(urls)
-    print(type(urls))
+    #Step 1. 각 딜의 html data 가져오기
+    req = requests.get(URL)
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
 
+    #Step 2. 각 딜의 필요정보 추출
+    
+    # 기본 상품 정보
+    title  = soup.find('div', class_='CxNYUPvHfB').get_text()
+    thumbnail = soup.find('img', class_='_2P2SMyOjl6')['data-src']
+    discount_rate = soup.find('span', class_='_1G-IvlyANt').get_text()[0:-3]
+    original_price = soup.find_all('span', class_='_1LY7DqCnwR')[0].get_text().replace(",","")
+    sale_price = soup.find_all('span', class_='_1LY7DqCnwR')[1].get_text().replace(",","")
+    delivery = soup.find('div', class_='pTaPzv0i6H').get_text().replace('택배배송', '택배배송 ')
+    try :
+        delivery_condition = soup.find('p', class_='_1JN0qDXZim').get_text()
+    except :
+        delivery_condition = 'No Condition'
+    
+    # 리뷰
+    num_reviews = soup.find_all('strong', class_='_2pgHN-ntx6')[0].get_text().replace(",","")
+    review_greade = soup.find_all('strong', class_='_2pgHN-ntx6')[1].get_text()[0:-2]
+    
+    print(title)
+    print(thumbnail)
+    print(discount_rate)
+    print(original_price)
+    print(sale_price)
+    print(delivery)
+    print(delivery_condition)
+    print(num_reviews)
+    print(review_greade)
+    print("==========================")
 
 if __name__ == "__main__":
     
